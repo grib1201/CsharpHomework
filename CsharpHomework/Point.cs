973b8aeede6543d5f4ef2b3ac1c79a7dff3c9f4b
Maria <mary.griboedova@gmail.com>
2022-12-02 09:52:00 +0100

    Added proj

---
+++ CsharpHomework/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHomework
+{
+    internal class Point
+    {
+        public Point()
+        {
+
+        }
+        public Point(int x, int y)
+        {
+
+        }
+
+        public int x;
+        public int y;
+
+        public Point Add(Point p1)
+        {
+            Point point = new Point();
+            point.x = x + p1.x;
+            point.y = y + p1.y;
+            return point;
+        }
+    }
+
+}
---
+++ CsharpHomework/Program.cs
@@ -0,0 +1,11 @@
+﻿using CsharpHomework;
+
+static void Main(string[] args)
+{
+    Point point = new Point(1,0);
+
+    Point point2 = new Point(2, 1);
+
+    Point point3 = point.Add(point2);
+    
+}
