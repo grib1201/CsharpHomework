e64c793abd9fb6749741102e4da5c33d6025cc46
Maria <mary.griboedova@gmail.com>
2023-04-03 16:14:16 +0200

    Added classes Color and Contour

---
+++ CsharpHomework/Color.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpHomework
+{
+    public class Color
+    {
+        private string _colorValue;
+
+        public Color(string colorValue)
+        {
+            this._colorValue = colorValue;
+        }
+    }
+}
